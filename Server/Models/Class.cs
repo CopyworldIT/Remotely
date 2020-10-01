38635fa6c28d86f58929329c253140d465775e56
MrElson <52317793+MrElson@users.noreply.github.com>
2020-10-01 17:12:34 +0930

    Attempt 1

---
+++ Server/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Remotely.Server.Models
+{
+    public class OSisx86
+    {
+        public bool X86 { get; set; }
+    }
+}
